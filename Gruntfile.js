951a108f60a4c42aed9b4eaf6b0c3e916c996909
ahvar <ahvargas92@gmail.com>
2016-07-29 21:44:38 -0400

    Google Maps API - Map Markers and Interactivity
    Add Markers for Morelia,MX and DF,MX
    Add buttons for zoom to DF and at a street level

---
+++ Gruntfile.js
@@ -0,0 +1,106 @@
+module.exports = function(grunt) {
+
+	var app_files = [],
+		output = 'dist/built.js',
+		test_output = 'test/built.js';
+
+	//project configuration
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+
+		copy: {
+			dist: {
+				cwd: 'src/',
+				expand: true,
+				src: '**',
+				dest: 'dist/'
+			}
+		},
+
+		inline: {
+			dist: {
+				options: {
+					inlineTagAttributes: {
+						js: 'data-inlined="true"',
+						css: 'data-inlined="true"'
+					},
+					cssmin: true,
+					uglify: true
+				},
+				src:'src/index.html',
+				dest:'dist/index.html'
+			}
+		},
+
+		htmlmin: {
+
+			options: {
+				removeComments: true,
+				collapseWhitespace: true
+			},
+
+			dist: {
+				files: [
+					{
+						expand: true,
+						cwd: 'dist/',
+						src: ['*.html'],
+						dest: 'dist/'
+					}
+				]
+			}
+		},
+
+		concat: {
+			options: {
+				separator: ';',
+			},
+			dist: {
+				src: app_files,
+				dest: output
+			},
+			test: {
+				src: app_files,
+				dest: test_output
+			}
+		},
+
+		mochaTest: {
+			test: {
+				options: {
+					reporter: 'spec'
+				},
+				src: ['test/**/*.js']
+			}
+		},
+
+		bowerInstall: {
+			dist: {
+				src: ['src/*.html'],
+				dependencies: true,
+				devDependencies: true,
+				exclude: []
+			}
+		},
+
+		wiredep: {
+			task: {
+				src: [
+					'src/*.html'
+				],
+				options: {
+
+				}
+			}
+		}
+
+	});
+
+	grunt.loadNpmTasks('grunt-wiredep');
+	grunt.loadNpmTasks('grunt-bower-install');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-htmlmin');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-mocha-test');
+	grunt.registerTask('default', ['copy','bower-install','concat','htmlmin']);	
+};
